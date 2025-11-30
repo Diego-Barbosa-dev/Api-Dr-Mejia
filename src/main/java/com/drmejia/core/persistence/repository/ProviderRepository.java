45c1b91915de566e31929158657bd1e969624181
Diego <alejitobarbosa09@gmail.com>
2025-11-30 14:23:20 -0500

    Cambio menor en el repositorio

--- src/main/java/com/drmejia/core/persistence/repository/ProviderRepository.java
+++ src/main/java/com/drmejia/core/persistence/repository/ProviderRepository.java
@@ -5,9 +5,8 @@ import org.springframework.stereotype.Repository;
 
 import com.drmejia.core.persistence.entities.ProviderEntity;
 
-import lombok.NonNull;
 
 @Repository
 public interface ProviderRepository extends JpaRepository<ProviderEntity, Long> {
-    void deleteById(@NonNull Long idProvider);
+    
 }
